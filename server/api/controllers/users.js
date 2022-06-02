da4ba1abee647d8eeaa92fc8540cc63100062ba7
mabus <m.buscaroli@outlook.com>
2022-06-02 17:25:57 +0100

    remove uuid module as mongo adds its own id, partially implemented GET /users

--- server/api/controllers/users.js
+++ server/api/controllers/users.js
@@ -3,7 +3,16 @@ const router = express.Router()
 
 const User = require('../models/user')
 
-router.get('/', (req, res) => {})
+router.get('/', async (req, res) => {
+  const payload = req.body
+  console.log('payload ', payload)
+  try {
+    const foundUser = await User.findByEmail(data)
+    res.send(foundUser)
+  } catch (err) {
+    res.status(404).send({ error: err })
+  }
+})
 
 router.post('/', async (req, res) => {})
 
--- server/api/models/user.js
+++ server/api/models/user.js
@@ -10,4 +10,28 @@ class User {
     this.password = password
     this.basket = []
   }
+
+  static findByEmail() {
+    return new Promise(async (resolve, reject) => {
+      console.log('data ', data)
+      try {
+        const db = await init()
+        const foundUser = await db
+          .collection('users')
+          .find({ email: data.email })
+          .toArray()
+        console.log('foundUser -> ', foundUser)
+        const user = new User({
+          name: foundUser.name,
+          email: foundUser.email,
+          password: foundUser.password,
+        })
+        resolve(user)
+      } catch (err) {
+        reject(`Error fetching user: ${err}`)
+      }
+    })
+  }
 }
+
+module.exports = User
